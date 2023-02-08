f3ba95dd3d8dbd3a27f3c95ce397987bb5a983ca
Pierre Quignon <p.quignon.pro@gmail.com>
2023-02-08 19:21:08 +0100

    fixed the problem with the header

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Header from './compoments/Header'
 
-import Home from './pages/Home';
-import Survey from './pages/Survey';
-import Header from './components/Header';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Header />
-        <Route exact path="/" element={<Home />}/>
-        <Route path="/survey" element={<Survey />}/>
-      </Routes>
+      <Header />
+        <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route path="/survey" element={<Survey />}/>
+        </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById('root')
+)
