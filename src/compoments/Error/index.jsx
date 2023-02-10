213ff999dda2b1d19fed81dba931d00e684d8b63
Pierre Quignon <p.quignon.pro@gmail.com>
2023-02-10 14:57:39 +0100

    finished the first part of the application

--- src/compoments/Error/index.jsx
+++ src/compoments/Error/index.jsx
@@ -1,8 +1,32 @@
+import styled from 'styled-components'
+import colors from '../../utils/style/color.js'
+
+const ErrorWrapper = styled.div`
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.background};
+  align-items: center;
+`
+
+const ErrorTitle = styled.h1`
+  font-weight: 300;
+  margin-top: 20px;
+`
+
+const ErrorSubtitle = styled.h2`
+  font-weight: 300;
+  color: ${colors.secondary};
+`
+
 function Error() {
   return (
-      <div>
-          <h1>Oups 🙈 Cette page n'existe pas</h1>
-      </div>
+      <ErrorWrapper>
+        <ErrorTitle>Oups...</ErrorTitle>
+        <ErrorSubtitle>
+          Il semblerait que la page que vous cherchez n'existe pas
+        </ErrorSubtitle>
+      </ErrorWrapper>
   )
 }
 
